a22a400ad92ba0f5f006e4bbdada2b2f9a6f226f
Kanav Arora <kanavarora1515@gmail.com>
2021-08-21 22:19:48 +0530

    Initial Commit

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*   It is a pre processor command. It will command compiler to import stdio.h file   */
+
+
+/*  Execution of C program always starts from the main function.
+    Return type of main function is int
+    It returns 0 to the compiler is the execution is successfull
+*/
+int main()
+{
+    printf("Hello World! My first program in C language");
+    return 0;
+}
