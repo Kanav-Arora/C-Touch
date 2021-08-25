3977bc131924cff799a1a8680db371be6ef272b9
Kanav Arora <kanavarora1515@gmail.com>
2021-08-25 22:27:45 +0530

    Typecasting added

---
+++ typecasting.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int main()
+{
+    int a = 5;
+    // 54/5 is an operation between to integers therefore we need to typecast it in float
+    float b = (float)54/5;
+    printf("Value of b is %f",b);
+    return 0;
+}
